a5773acb071ca7d39ecdf8fae37e9db1bc1270e6
john <helius.thu@gmail.com>
2014-07-13 01:50:08 +0800

    table added

--- static/website/js/query_data_daily.js
+++ static/website/js/query_data_daily.js
@@ -1,3 +1,13 @@
 $(function(){
     $('.date_picker').datepicker($.datepicker.regional[ "zh-TW" ]);
+    $('#mytable').dataTable({
+        "language": {
+            "lengthMenu": "每页显示 _MENU_ 条记录",
+            "zeroRecords": "没有任何数据",
+            "info": "当前页面 _PAGE_ 页面数 _PAGES_",
+            "infoEmpty": "没有任何数据",
+            "infoFiltered": "(filtered from _MAX_ total records)",
+            "search": "搜索"
+        }
+    });
 });
