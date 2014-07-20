73becf7d9ae5314baf6360bde6a51ae0fecefb8b
john <helius.thu@gmail.com>
2014-07-20 22:17:11 +0800

    query_date, javascript logic setting

--- static/website/js/query_data_daily.js
+++ static/website/js/query_data_daily.js
@@ -1,5 +1,8 @@
-$(function(){
+function datepicker_init() {
     $('.date_picker').datepicker($.datepicker.regional[ "zh-TW" ]);
+}
+
+function dataTable_init() {
     $('#mytable').dataTable({
         "language": {
             "lengthMenu": "每页显示 _MENU_ 条记录",
@@ -10,4 +13,56 @@ $(function(){
             "search": "搜索"
         }
     });
+}
+
+function select_init() {
+    $('#monitor_type').change(function() {
+        var mt = $(this).val();
+        /* 恒电位仪*/
+        if (mt == 'h') {
+            // step 1. 在查询类型中加入"电位"
+            $('#search_type').append('<option value="potential">电位</option>');
+            // step 2. 把"数据状态"禁止掉
+            
+        /* 采集器 */    
+        } else if (mt == 'c') {
+            // step 1. 在查询类型中删除"电位"
+            $('#search_type option[value="potential"]').remove();
+        }
+        //alert($(this).find('option:selected').text());
+    });
+}
+
+function query_init() {
+    $('#search_btn').bind('click', function(){
+        /* step 1. 检查输入 */
+        var start_date = $('#start_date').val();
+        var end_date = $('#end_date').val();
+        if (!isValidDate(start_date) || !isValidDate(end_date)) {
+            alert('日期输入有误, 请检查！');
+            return;
+        }
+        if (start_date > end_date) {
+            alert('开始日期必须不超过结束日期');
+            return;
+        }
+        /* step 2. 获得表单值 */
+        var monitor_type = $('#monitor_type').val();
+        var search_type = $('#search_type').val();
+        var data_status = $('#data_status').val();
+        var node = $('#node_list').val();
+        alert(start_date);
+        alert(end_date);
+        alert(monitor_type);
+        alert(search_type);
+        alert(data_status);
+        alert(node);
+    });
+}
+
+$(function(){
+    datepicker_init();
+    dataTable_init();
+    select_init();
+    query_init();
 });
---
+++ static/website/js/util.js
@@ -0,0 +1,12 @@
+function isValidDate(date)
+{
+    var matches = /^(\d{4})[/](\d{2})[/](\d{2})$/.exec(date);
+    if (matches == null) return false;
+    var d = matches[3];
+    var m = matches[2] - 1;
+    var y = matches[1];
+    var composedDate = new Date(y, m, d);
+    return composedDate.getDate() == d &&
+            composedDate.getMonth() == m &&
+            composedDate.getFullYear() == y;
+}
