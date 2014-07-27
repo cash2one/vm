3bb50b81a7e4650754d3b7cb1336ea81eac3ea79
wujian <wujiankaka@126.com>
2014-07-27 17:16:20 +0800

    haha

--- website/urls.py
+++ website/urls.py
@@ -14,20 +14,16 @@ urlpatterns = patterns('',
     (r'^node_manage$', views_page.node_manage), #节点管理
 )
 
-urlpatterns += patterns('',
-    (r'^get_data_hourly$', getDataHourly), 
-)
-
 import views_account
 urlpatterns += patterns('',
         (r'^login_page/$', TemplateView.as_view(template_name="website/login.html")),#登录页面
-	(r'^login/$', views_account.login_ajax),#登录函数
-	(r'^logout/$', views_account.logout_ajax),#注销函数
+        (r'^login/$', views_account.login_ajax),#登录函数
+        (r'^logout/$', views_account.logout_ajax),#注销函数
 )
 
 import views
 urlpatterns += patterns('',
-    (r'^node_all_location', views.node_all),
+    (r'^node_all_location$', views.node_all),
     (r'^get_data_hourly$', views.getDataHourly),
     (r'^get_node_desc$', views.getNodeDesc), 
 )
